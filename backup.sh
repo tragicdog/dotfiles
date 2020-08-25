6cc17b34ca0d15ca1025be0419478538cd9bb6a4
Jonathon Lillie <jonlillie@gmail.com>
2020-08-25 15:51:11 -0700

    added backup script from https://dev.to/jeffshomali/how-to-backup-sync-all-of-your-dotfiles-with-github-e1c

---
+++ backup.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+#check to see if git command line is installed in this machine
+IS_GIT_AVAILABLE="$(git --version)"
+if [[ $IS_GIT_AVAILABLE == *"version"* ]]; then
+  echo "Git is available"
+else
+  echo "Git is not installed"
+  exit 1
+fi
+
+#copy VS_Code files
+p $HOME/Library/Application\ Support/Code/User/{keybindings.json,settings.json,spellright.dic} .
+
+cp -r $HOME/Library/Application\ Support/Code/User/snippets .
+
+code --list-extensions --show-versions >> ../..vscode/extensions.txt
+
+#copy other dot files
+cp $HOME/{.zshrc, .vimrc, .vscode, .tmux.conf} .
+
+#check git status
+gs="$(git status | grep -i "modified")"
+# echo "${gs}"
+
+# If there is a new change
+if [[ $gs == *"modified"* ]]; then
+  echo "push"
+fi
+
+# push to Github
+git add -u;
+git commit -m "new backup `date +'%Y-%m-%d %H:%M:%S'`";
+git push origin master
